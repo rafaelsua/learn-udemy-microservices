2395e009ee4e465974240ad01245a7bf272d5214
rsuarez <rsuarez@ITEM-S29009.emea.msad.sopra>
2017-06-22 10:11:58 +0200

    Intial Commit

---
+++ lab-3-server/src/test/java/com/dhaker/microservices/lab3/server/Lab3ServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dhaker.microservices.lab3.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lab3ServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
