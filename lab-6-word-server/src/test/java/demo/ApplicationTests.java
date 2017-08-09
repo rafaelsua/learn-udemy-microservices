bbeb2c0273e048f4ac14561ccee5401b2c564dca
rsuarez <rsuarez@ITEM-S29009.emea.msad.sopra>
2017-08-09 14:46:36 +0200

    Delete imports

--- lab-6-word-server/src/test/java/demo/ApplicationTests.java
+++ lab-6-word-server/src/test/java/demo/ApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import demo.Application;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class ApplicationTests {
