9f39d80201f391e01e384da8d4e69cee6062c6d3
rsuarez <rsuarez@ITEM-S29009.emea.msad.sopra>
2017-08-25 00:25:35 +0200

    change the name of package and the config data to spring bus properties

--- lab-8-client-own/src/test/java/com/dhaker/microservices/ApplicationTests.java
+++ lab-8-client-own/src/test/java/com/dhaker/microservices/ApplicationTests.java
@@ -1,9 +1,11 @@
-package demo;
+package com.dhaker.microservices;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.dhaker.microservices.Application;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
