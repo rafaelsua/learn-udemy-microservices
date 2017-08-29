8e5b4b253c48bb02494fa57c1473130da31f3d59
rsuarez <rsuarez@ITEM-S29009.emea.msad.sopra>
2017-08-29 23:38:47 +0200

    Test Spring Cloud Bus

--- lab-8-client-own/src/main/java/com/dhaker/microservices/LuckyWordController.java
+++ lab-8-client-own/src/main/java/com/dhaker/microservices/LuckyWordController.java
@@ -1,20 +1,30 @@
 package com.dhaker.microservices;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @ConfigurationProperties(prefix = "wordConfig")
 public class LuckyWordController {
 
     String luckyWord;
     String preamble;
+    String fullStatement;
+
+    @PostConstruct
+    public void init() {
+        fullStatement = preamble + ": "
+            + luckyWord;
+    }
 
     @RequestMapping("/lucky-word")
     public String showLuckyWord() {
-        return preamble + ": "
-            + luckyWord;
+        return fullStatement;
     }
 
     public String getLuckyWord() {
